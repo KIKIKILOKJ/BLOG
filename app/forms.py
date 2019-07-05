b089ed5b4b252d209653c7136b5aa2c908e26bd9
peter max <petermax2004@gmail.com>
2019-07-05 16:16:22 +0300

    Add the comment.html file

--- app/forms.py
+++ app/forms.py
@@ -1,3 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,TextAreaField,SubmitField
 from wtforms.validators import Required
+
+
+class CommentForm(FlaskForm):
+    name = StringField('Name', validators=[Required()])
+    comment = TextAreaField('Blog comment', validators=[Required()])
+    submit = SubmitField('Submit')
