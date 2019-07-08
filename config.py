226481b9fe48075421bb5954748bd32108f7b78d
peter max <petermax2004@gmail.com>
2019-07-08 09:14:38 +0300

    Add the email configurations

--- config.py
+++ config.py
@@ -5,11 +5,17 @@ class Config:
     General configuration parent class
     '''
     QUOTES_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
-    pass
+    
+    #email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 class ProdConfig(Config):
     '''
-    Pruduction configuration child class
+    Production configuration child class
 
     Args:
         Config: The parent configuration class with General configuration settings
