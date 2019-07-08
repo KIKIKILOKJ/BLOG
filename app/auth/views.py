6f60daf667269d080f109d0882a42f2d9260e18a
peter max <petermax2004@gmail.com>
2019-07-08 09:01:10 +0300

    Add thelogin required import

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,7 +1,7 @@
 from . import auth
 from .. import db
 from flask import render_template, redirect, url_for, flash, request
-from flask_login import login_user, logout_user, 
+from flask_login import login_user, logout_user, login_required
 from ..models import User
 from .forms import LoginForm, RegistrationForm
 
