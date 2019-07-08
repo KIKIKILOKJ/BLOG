4df51717b9b053105aadabe9c27f7ddd1bf47e89
peter max <petermax2004@gmail.com>
2019-07-08 10:11:30 +0300

    Add the start.sh file

--- manage.py
+++ manage.py
@@ -37,4 +37,5 @@ def make_shell_context():
 
 
 if __name__ == '__main__':
+    app.secret_key='abcdef'
     manager.run()
---
+++ start.sh
@@ -0,0 +1,6 @@
+export MAIL_USERNAME='petermax2004@gmail.com'
+export MAIL_PASSWORD='RAJIV12345'
+export DATABASE_URI ='postgresql+psycopg2://peter:abcdef@localhost/blogs'
+
+
+python3.6 manage.py server
